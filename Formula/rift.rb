ff85eed899e458f701dabe8de6906d5e26e85f64
Jarrett Chen <jarrettc124@gmail.com>
2026-04-24 11:16:39 -0700

    rift 0.2.0

--- Formula/rift.rb
+++ Formula/rift.rb
@@ -7,28 +7,28 @@
 class Rift < Formula
   desc "Secure tunnels to localhost — self-hosted ngrok alternative"
   homepage "https://github.com/wyrld-org/homebrew-tap/tree/main/apps/rift"
-  version "0.1.0"
+  version "0.2.0"
   license "MIT"
 
   on_macos do
     on_arm do
-      url "https://github.com/wyrld-org/homebrew-tap/releases/download/rift-v0.1.0/rift-0.1.0-darwin-arm64.tar.gz"
-      sha256 "f6cf37999f7aaed9590f9af505b2afc7a6e600626db7b4daf8243c638ca3878d"
+      url "https://github.com/wyrld-org/homebrew-tap/releases/download/rift-v0.2.0/rift-0.2.0-darwin-arm64.tar.gz"
+      sha256 "c736f8b533ec69b2364bba414d0e38afdb558f63ff9743eead2ccf3546b8f71c"
     end
     on_intel do
-      url "https://github.com/wyrld-org/homebrew-tap/releases/download/rift-v0.1.0/rift-0.1.0-darwin-amd64.tar.gz"
-      sha256 "876a800d700ecce76789c90e272b920c3d5ca606dad86d9974db8b613f99a315"
+      url "https://github.com/wyrld-org/homebrew-tap/releases/download/rift-v0.2.0/rift-0.2.0-darwin-amd64.tar.gz"
+      sha256 "53295a13289a8ad6da610db7ec136119aee72d4fecbacc06d24f6488760a60cd"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://github.com/wyrld-org/homebrew-tap/releases/download/rift-v0.1.0/rift-0.1.0-linux-arm64.tar.gz"
-      sha256 "7f44e34350a06ce6fdd8658a585d3ac7390f730554dcdc50f14442686b3a6f7a"
+      url "https://github.com/wyrld-org/homebrew-tap/releases/download/rift-v0.2.0/rift-0.2.0-linux-arm64.tar.gz"
+      sha256 "0552852e1dd554969bfdf99b8b4e2f13f5591cef90393f1b66b3c83f114edfde"
     end
     on_intel do
-      url "https://github.com/wyrld-org/homebrew-tap/releases/download/rift-v0.1.0/rift-0.1.0-linux-amd64.tar.gz"
-      sha256 "6e6cee95dfb010f1c3b6e132d8fd98ad9f5fc3b466d142169597c04d65544608"
+      url "https://github.com/wyrld-org/homebrew-tap/releases/download/rift-v0.2.0/rift-0.2.0-linux-amd64.tar.gz"
+      sha256 "ba1f59d494571334b1f8412db778a7a30a9e2e982d62835a5fb0f4f731f65261"
     end
   end
 
